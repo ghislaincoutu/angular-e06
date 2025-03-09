85696ca44a310979de6e2959c6256e9dc88e21ad
gcoutu <gc8395@gmail.com>
2025-03-08 23:20:32 -0500

    Exercice Angular

--- related_files/scripts/backup_angular-e06.sh
+++ related_files/scripts/backup_angular-e06.sh
@@ -6,18 +6,18 @@ echo "Copie de sécurité des fichiers relatifs à l'application angular12e04"
 apply_pause "Appuyer sur la touche [Retour] pour continuer..."
 
 source=/var/www/html/d002/angular-e06
-destination=/home/Exercises/angular-e06/related_files/publication
+destination=/home/xd21/angular-e06/related_files/publication
 rm $destination/*.tar.gz
 tar -czvf $destination/pub_angular-e06_`(date +%Y%m%d-%H%M)`.tar.gz $source/
 
 source=/var/www/html/d002/angular-e06t01/
-destination=/home/Exercises/angular-e06/related_files/publication/pub_angular-e06t01_`(date +%Y%m%d-%H%M)`.tar.gz
+destination=/home/xd21/angular-e06/related_files/publication/pub_angular-e06t01_`(date +%Y%m%d-%H%M)`.tar.gz
 tar -czvf $destination $source
 
 source=/var/www/html/d002/angular-e06t03/
-destination=/home/Exercises/angular-e06/related_files/publication/pub_angular-e06t03_`(date +%Y%m%d-%H%M)`.tar.gz
+destination=/home/xd21/angular-e06/related_files/publication/pub_angular-e06t03_`(date +%Y%m%d-%H%M)`.tar.gz
 tar -czvf $destination $source
 
-source=/home/Exercises/angular-e06/
+source=/home/xd21/angular-e06/
 destination=/home/Backups/angular-e06_`(date +%Y%m%d-%H%M)`.tar.gz
 tar --exclude=".angular" --exclude=".angular" --exclude=".git" --exclude="node_modules" -czvf $destination $source
--- related_files/scripts/publish_application.sh
+++ related_files/scripts/publish_application.sh
@@ -8,7 +8,7 @@ echo "Publication de l'applications sur le serveur Web local"
 apply_pause "Appuyer sur la touche [Retour] pour continuer..."
 
 # Source (so)
-so=/home/Exercises/angular-e06/dist/angular-e06/browser
+so=/home/xd21/angular-e06/dist/angular-e06/browser
 # Destination (de)
 de=/var/www/html/d002/angular-e06
 
